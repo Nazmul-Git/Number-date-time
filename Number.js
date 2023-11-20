e1b104bc7246d9c2291f257ffa42fa9d0b32ed04
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-21 01:27:25 +0600

    Number conversion & parsing.

---
+++ Number.js
@@ -0,0 +1,16 @@
+console.log(0.1 + 0.2); // 0.30000000000000004
+console.log(0.1 + 0.2 === 0.3); //false
+
+
+// Conversion
+console.log(Number('23')); // 23
+console.log( +'23' ); //23
+
+// Parsing
+console.log(Number.parseInt('30px', 2)); // NaN
+console.log(Number.parseInt('30px', 10)); // 30
+console.log(Number.parseInt('e303', 10));  // NaN
+
+console.log(Number.parseInt('20 $')); // 20
+console.log(Number.parseInt('20.7 $')); // 20
+console.log(Number.parseFloat('20.7 $')); // 20.7
