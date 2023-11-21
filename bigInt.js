5f1760edde82449da09d28494964065cd019a899
Nazmul-Git <130184909+Nazmul-Git@users.noreply.github.com>
2023-11-22 02:07:18 +0600

    For big number use bigInt..

---
+++ bigInt.js
@@ -0,0 +1,11 @@
+ console.log(2 ** 57 - 1); //144115188075855870
+ console.log(2 ** 57 + 2); //144115188075855870
+ console.log(2 ** 57 + 3); //144115188075855870
+
+ console.log(BigInt(5678686655861)); //5678686655861n
+ console.log(1000n + 1000n); //2000n
+
+ const huge=5454456454245445414n;
+ const num=26;
+//  console.log(huge+num); //TypeError: Cannot mix BigInt and other types
+console.log(huge+BigInt(num)); //5454456454245445440n
